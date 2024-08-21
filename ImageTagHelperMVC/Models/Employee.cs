58049caca0c3a5ef1a83f35fffed6951e11659a0
waqar <wakku9780@gmail.com>
2024-08-22 01:12:06 +0530

    Add project files.

---
+++ ImageTagHelperMVC/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace ImageTagHelperMVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }       // Unique identifier for the employee
+        public string Name { get; set; }    // Employee's name
+        public Gender gender { get; set; }
+        public string Position { get; set; }      // Job title or position
+        public string Department { get; set; }    // Department the employee belongs to
+        public string Email { get; set; }         // Employee's email address
+        public string Phone { get; set; }         // Employee's phone number
+        public DateTime DateOfBirth { get; set; } // Employee's date of birth
+        public DateTime DateOfJoining { get; set; } // Date when the employee joined the company
+        public decimal Salary { get; set; }       // Employee's salary
+        public string Address { get; set; }       // Employee's residential address
+    }
+
+    public enum Gender
+    {
+        Male,Female
+    }
+}
---
+++ ImageTagHelperMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageTagHelperMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
